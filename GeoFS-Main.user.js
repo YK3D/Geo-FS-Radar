b5e774964fa61b2ac67e76008c0709132d135169
YK3D <joemasoud2010@gmail.com>
2026-03-01 17:58:52 +0200

    PASTE THIS INTO TAMPERMONNKEY
    This user script is designed for automatic updates of the Geo-FS radar.

---
+++ GeoFS-Main.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Geo-FS Radar Auto Update
+// @namespace    http://tampermonkey.net/
+// @version      8.06
+// @description
+// @author       YK3D
+// @match        https://www.geo-fs.com/geofs.php?v=3.9
+// @grant        none
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
+// @require      https://purge.jsdelivr.net/gh/YK3D/Geo-FS-Radar@main/Geo-FS-Radar.user.js
+// ==/UserScript==
+
+// This file is intentionally empty - all code is in the @require URL
+// The @require will execute on every page load with fresh code!
