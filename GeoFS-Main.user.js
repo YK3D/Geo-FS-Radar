00bf41ecb1b7b884795403ac430a25af58fffa62
YK3D <joemasoud2010@gmail.com>
2026-03-01 19:30:47 +0200

    PASTE THIS INTO TAMPERMONNKEY
    This script is intentionally empty, it only contains metadata.
    @require allows it to automatically update whenever I make changes so you don't have to worry about re-installing

--- GeoFS-Main.user.js
+++ GeoFS-Main.user.js
@@ -2,13 +2,17 @@
 // @name         Geo-FS Radar Auto Update
 // @namespace    http://tampermonkey.net/
 // @version      8.06
-// @description
+// @description  Radar for GeoFS
 // @author       YK3D
 // @match        https://www.geo-fs.com/geofs.php?v=3.9
 // @grant        none
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
-// @require      https://purge.jsdelivr.net/gh/YK3D/Geo-FS-Radar@main/Geo-FS-Radar.user.js
+// @require      https://cdn.jsdelivr.net/gh/YK3D/Geo-FS-Radar@main/Geo-FS-Radar.user.js
 // ==/UserScript==
 
-// This file is intentionally empty - all code is in the @require URL
-// The @require will execute on every page load with fresh code!
+/* This space intentionally left blank — all code is loaded via @require
+The @require will execute on every page load with fresh code! 
+Any changes I make will automatically update on your end!*/
+
+
+//https://github.com/YK3D/Geo-FS-Radar/tree/main
