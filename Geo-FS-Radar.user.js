e4c36a7a5dd4a0cf62ee3d1329055d91d437f01f
YK3D <joemasoud2010@gmail.com>
2026-03-03 16:40:23 +0200

    Update Geo-FS-Radar.user.js

--- Geo-FS-Radar.user.js
+++ Geo-FS-Radar.user.js
@@ -125,7 +125,7 @@ const UI = {
     ringLabelFont:       15,
 
     // ── Compass cardinal letters ──────────────────────────────────────────
-    compassFont:         30,
+    compassFont:         50,
     compassHdgFont:      14,
 
     // ── Own-aircraft triangle ─────────────────────────────────────────────
